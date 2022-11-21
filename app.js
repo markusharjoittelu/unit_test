519474111235f70abcd79724de6c760b3fd7b23e
Markus <niiranen.markus@gmail.com>
2022-11-21 10:36:32 +0200

    Portti fixi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const calculator = require('./calculator')
-const port = 3000;
+//const port = 3000;
 
 
 app.get('/',function (request,response){
@@ -15,7 +15,7 @@ app.get('/calculate/:x/:y',function (request,response){
         response.json(result);
     }
     );
-
+var port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
